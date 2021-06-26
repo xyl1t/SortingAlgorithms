c3259834c0376e7788fe90cd5f975f27c89781fc
Marat Isaw <marat.isaw23@gmail.com>
2021-06-26 18:02:39 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#if __has_include("SDL2/SDL.h")
+# include <SDL2/SDL.h>
+#else
+# include "SDL.h"
+#endif
+#define CIDR_IMPLEMENTATION
+#include "cidr.hpp"
+
+int main(int argc, char** argv) {
+	
+	return 0;
+}
